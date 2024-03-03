b28b92081b3cdecc462a1acf1c9c49a0acc0f30d
davidade300 <iaderinho@outlook.com>
2024-03-03 19:45:52 -0300

    corrigindo commit

---
+++ Class&MetodoAbstratos/src/entities/SavingsAccount.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class SavingsAccount extends Account{
+	
+	private Double interestRate;
+	
+	public SavingsAccount() {
+		super();
+	}
+
+	public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+		super(number, holder, balance);
+		this.interestRate = interestRate;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public void updateBalance() {
+		balance +=  balance * interestRate;
+	}
+
+	// adicionando final ao metodo, classes que herdem esta classe não poderão sobrepor esse método
+	@Override
+	public final void withdraw(double ammount) {
+		balance -= ammount;
+	}
+	
+}
