cdf5a636dd3a469b083a336b67263b1900b4acbc
davidade300 <iaderinho@outlook.com>
2024-03-06 20:41:34 -0300

    commit do dia

--- Excecoes/src/application/Program.java
+++ Excecoes/src/application/Program.java
@@ -6,7 +6,20 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		method1();
 
+		System.out.println("End of program");
+	}
+	
+	public static void method1() {
+		System.out.println("***METHOD 1 START ***");
+		method2();
+		System.out.println("***METHOD 1 END***");
+	}
+	
+	public static void method2() {
+		System.out.println("***METHOD 2 START ***");
 		Scanner sc = new Scanner(System.in);
 		try {
 			String[] vect = sc.nextLine().split(" ");
@@ -15,14 +28,16 @@ public class Program {
 		}
 		catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("invalid position !");
+			e.printStackTrace();
+			sc.next();
 		}
 		catch (InputMismatchException e) {
 			System.out.println("Input error");
 		}
 		
-		System.out.println("End of program");
+		
 		
 		sc.close();
+		System.out.println("***METHOD 2 END ***");
 	}
-
 }
--- LeaoPolimorfismoEx/src/application/Progam.java
+++ LeaoPolimorfismoEx/src/application/Progam.java
@@ -1,11 +1,61 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import Entities.Company;
+import Entities.Individual;
+import Entities.TaxPayer;
+
 public class Progam {
 
 	public static void main(String[] args) {
  
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		List<TaxPayer> list = new ArrayList<>();
+		
+		System.out.print("Enter the number of tax payers: ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Individual or company (i/c)? ");
+			char c = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Anual income: ");
+			double income = sc.nextDouble();
+			switch (c) {
+			case 'i':
+				System.out.print("Health expenditures: ");
+				double healthExpenditures = sc.nextDouble();
+				list.add(new Individual(name, income, healthExpenditures));
+				break;
+			case 'c':		
+				System.out.print("Number of employees: ");
+				int numberOfEmployees = sc.nextInt();
+				list.add(new Company(name, income, numberOfEmployees));
+				break;
+			}		
+		}
+		
+		double sum = 0.0;
+		System.out.println();
+		System.out.println("TAXES PAID:");
+		for (TaxPayer tp : list) {
+			double tax = tp.tax();
+			System.out.println(tp.getName() + ": $ " + String.format("%.2f", tax));
+			sum += tax;
+		}
+		
+		System.out.println();
+		System.out.println("TOTAL TAXES: $ " + String.format("%.2f", sum));
 		
 		
+		sc.close();
 	}
 
 }
